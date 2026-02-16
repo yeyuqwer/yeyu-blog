e3118aac64dd4a232c96ff57a5a526f9ed15c2a8
Ye Yu <nearjilt@gmail.com>
2026-02-16 09:20:41 +0800

    chore(deps): 🪛 upgrade prisma to v7

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,16 @@
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
