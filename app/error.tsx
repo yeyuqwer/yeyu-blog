8f2ac85ed61cff0c05562e3ad8d3f69a74af20f3
Ye Yu <nearjilt@gmail.com>
2026-04-16 15:21:28 +0800

    refactor: 🥹 add error page back function

--- app/error.tsx
+++ app/error.tsx
@@ -1,14 +1,19 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { Button } from '@/ui/shadcn/button'
 
 export default function AppError() {
+  const router = useRouter()
+
   return (
     <section className="flex h-screen w-screen flex-col items-center justify-center gap-2 text-zinc-600 dark:text-zinc-300">
       <p className="text-3xl">(҂ ꒦ິヮ꒦ິ)</p>
       <h1 className="mt-2 font-medium text-xl">Something went wrong</h1>
       <p className="mt-2 font-medium text-xl">喵呜 ??</p>
-      <Button type="button">go back</Button>
+      <Button type="button" onClick={() => router.back()}>
+        go back
+      </Button>
     </section>
   )
 }
